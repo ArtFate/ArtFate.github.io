04380b28f817b091443d90c508c62fe5078df972
ArtFate <alfate@qq.com>
2024-08-19 11:36:50 +0800

    Site updated: 2024-08-19 11:36:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/af5a8a6c8d.html","posts/afe2bae1fb.html","posts/af87c0ee8.html","posts/af15175.html"];function toRandomPost(){
+var posts=["posts/afe2bae1fb.html","posts/af5a8a6c8d.html","posts/af87c0ee8.html","posts/af15175.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
